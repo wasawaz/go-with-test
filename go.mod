81f1fc3d8100e28f2f85849362a99f95dde59641
Wasawat Pruthipunyaskul <wasawatp@NBAAP2100004.local>
2022-08-12 21:29:31 +0700

    init: initial project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wasawaz/go-with-test
+
+go 1.18
---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Hello(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
